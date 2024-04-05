c8a272fe6f95646321069614aac81177e641cf0f
SamColt36 <operator.samuelcolt@gmail.com>
2024-04-05 20:38:52 -0300

    fix: remover node_modules/

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -1,17 +1,17 @@
-import { createClient } from "../../node_modules/pexels/dist/main.js";
+import { createClient } from "/node_modules/pexels/dist/main.js";
 
 const client = createClient(
   "IvtJEQNWC4iFB0UrtKN5jCpbamoJjhMUvWRp28gHUn5u2IrHFEQeXOE8"
 );
 
 const getImages = async () => {
-  try {
-    const getRandomNumber = (min, max) => {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
+  const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
+  try {
     const requestImage = await client.photos.search({
-      query: "nature",
+      query: "forest",
       orientation: "landscape",
       size: "medium",
       per_page: 4,
